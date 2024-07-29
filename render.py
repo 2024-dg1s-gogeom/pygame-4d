b40ffc1408973112ade662b4c90f5e016e6379b0
tiger2588j <tiger2588j@naver.com>
2024-07-29 15:49:58 +0900

    aa

--- render.py
+++ render.py
@@ -1,12 +1,16 @@
 import pygame
 from pygame.locals import *
+from map import show_map
 
-def front_render(font, display_surf):
+def front_render(font, maze, display_surf):
     display_surf.fill((0, 0, 0))
     title = font.render("4D Maze", True, (255, 255, 255))
     display_surf.blit(title, (400, 300))
     pygame.display.flip()
 
-def play_render(font, display_surf):
+def play_render(font, maze, display_surf):
     display_surf.fill((255, 255, 255))
+    for i in range(4):
+        show_map(i)
     pygame.display.flip()
+#
