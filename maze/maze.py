56e2a3bfbde49b98e16632ab8b40d4c633c54ecf
[GithubEF0818] <[Githubef20080118@gmail.com]>
2024-07-30 22:28:40 +0900

    message

--- maze/maze.py
+++ maze/maze.py
@@ -2,7 +2,11 @@ import numpy as np
 import pygame
 
 class Maze:
+    ##### 맵 시작 알고리즘 #####
     n = int(input("Enter the size of Tesseract(size of thes side)"))
+    global path
+    path = [[[[0 for x in range(n)] for y in range(n)] for z in range(n)] for w in range(n)] # 0: 막힌 길, 1: 뚫린 길, 2: 시작점, 3: 도착점
+
     def __init__(self, n):
         self.pos = np.array([0, 0, 0, 0])
         self.size = n
@@ -13,10 +17,10 @@ class Maze:
         coords_list = coords.tolist()
         return coords_list
     
+    ##### 길 생성 알고리즘 #####
     def generateMaze(self, n):
-        path = [[[[0 for x in range(n)] for y in range(n)] for z in range(n)] for w in range(n)] # 0: 막힌 길, 1: 뚫린 길, 2: 시작점, 3: 도착점
 
-        # 길 생성 알고리즘. 이 부분은 임의로 수정 가능 (일단 지금은 가장 기초적인 모델)
+    ### 가장 기초적인 길 모델 (추후 임의로 수정 가능) ###
         for i in range(n):          
             path[i][0][0][0] = 1
         for j in range(n):
@@ -26,7 +30,6 @@ class Maze:
         for w in range(n):
             path[n-1][n-1][n-1][w] = 1
 
-        # 도착지잠 / 시작지점 결정 알고리즘
         path[0][0][0][0] = 2
         path[n-1][n-1][n-1][n-1] = 3
 
