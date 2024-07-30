bf10a4cd770515f5659d3104fe62242e21b5a51d
tiger2588j <tiger2588j@naver.com>
2024-07-30 14:53:47 +0900

    efasdf

--- main.py
+++ main.py
@@ -11,52 +11,47 @@ class App:
         self._display_surf = None
         self.size = self.width, self.height = 800, 600
         self.font = pygame.font.Font(pygame.font.get_default_font(), 36)
- 
+
+
     def on_init(self):
         self._display_surf = pygame.display.set_mode(self.size, pygame.HWSURFACE | pygame.DOUBLEBUF)
         self._running = True
         self.maze = Maze(100)
- 
+        self.k = 0
+
+
     def on_event(self, event):
         if event.type == pygame.QUIT:
             self._running = False
 
     def on_loop(self):
-        self.render = render.front_render 
-        keys = pygame.key.get_pressed()
-        if self.render == render.front_render and keys[pygame.K_SPACE]:
+        for event in pygame.event.get():
+            self.on_event(event)  # 이벤트 처리
+            
+            if event.type == pygame.KEYDOWN and self.render == render.front_render:  # 키가 눌렸을 때
+                if event.key == pygame.K_SPACE:  # 스페이스바가 눌렸는지 확인
+                    self.k += 1
+        if self.k == 0:
+            self.render = render.front_render
+        else:
             self.render = render.play_render
-        self.playerpos = [0, 0, 0, 0]
-        modifier = 1
-        if keys[pygame.K_LSHIFT] or keys[pygame.K_RSHIFT]:    # SHIFT 누를 때 마다 앞 뒤 이동 방향 바뀜
-            modifier *= -1
-            if keys[pygame.K_q]:
-                self.playerpos[0] += modifier * 1
-            if keys[pygame.K_w]:
-                self.playerpos[1] += modifier * 1
-            if keys[pygame.K_e]:
-                self.playerpos[2] += modifier * 1
-            if keys[pygame.K_r]:
-                self.playerpos[3] += modifier *1
-
+    
     def on_render(self):
         self.render(self.font, self.maze, self._display_surf)
 
     def on_cleanup(self):
         pygame.quit()
- 
+
     def on_execute(self):
         if self.on_init() == False:
             self._running = False
  
-        while( self._running ):
-            for event in pygame.event.get():
-                self.on_event(event)
+        while self._running:
             self.on_loop()
             self.on_render()
         self.on_cleanup()
- 
-if __name__ == "__main__" :
+
+if __name__ == "__main__":
     theApp = App()
     theApp.on_execute()
 
