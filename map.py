eae7f5d1efb450b074bd382f3e3f24948fa86022
tiger2588j <tiger2588j@naver.com>
2024-07-31 14:33:37 +0900

    adfadsf

--- map.py
+++ map.py
@@ -6,12 +6,28 @@ import render
 def show_map(display_surf):
     drawList = [[200,150], [600,150], [200,450], [600,450]]
     white = ((255, 255, 255))
+    black = ((0, 0, 0))
     for a,b in drawList:
-
-        #side line
         pygame.draw.line(display_surf, white, [a+85, b+65], [a+155, b+115], 2)
         pygame.draw.line(display_surf, white, [a+85, b-65], [a+155, b-115], 2)
-        pygame.draw.line(display_surf, white, [a-75, b+55], [a-155, b+115], 2)
-        pygame.draw.line(display_surf, white, [a-75, b-65], [a-155, b-115], 2)
+        pygame.draw.line(display_surf, white, [a-85, b+65], [a-155, b+115], 2)
+        pygame.draw.line(display_surf, white, [a-85, b-65], [a-155, b-115], 2)
+
+        pygame.draw.line(display_surf, white, [a-85, b-65], [a+85, b-65], 2)
+        pygame.draw.line(display_surf, white, [a-85, b+65], [a+85, b+65], 2)
+        pygame.draw.line(display_surf, white, [a-155, b-115], [a+155, b-115], 2)
+        pygame.draw.line(display_surf, white, [a-155, b+115], [a+155, b+115], 2)
+        pygame.draw.line(display_surf, white, [a-85, b-65], [a-85, b+65], 2)
+        pygame.draw.line(display_surf, white, [a+85, b-65], [a+85, b+65], 2)
+        pygame.draw.line(display_surf, white, [a-155, b-115], [a-155, b+115], 2)
+        pygame.draw.line(display_surf, white, [a+155, b-115], [a+155, b+115], 2)
+
+    pygame.draw.line(display_surf, white, [355,265], [445,265], 2)
+    pygame.draw.line(display_surf, white, [355,335], [445,335], 2)
+    pygame.draw.line(display_surf, white, [355,265], [355,335], 2)
+    pygame.draw.line(display_surf, white, [445,265], [445,335], 2)
+
+    pygame.draw.line(display_surf, black, [400,265], [400,335], 3)
+    pygame.draw.line(display_surf, black, [355,300], [445,300], 3)
         
     
