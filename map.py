8c2e83afd0f177a85f812997b32d5b59db2f5109
liv8lt <dlehddbs1113@gmail.com>
2024-07-31 15:22:17 +0900

    qq

--- map.py
+++ map.py
@@ -14,29 +14,22 @@ def show_map(display_surf):
 
         #cross line
         pygame.draw.line(display_surf, white, (400,0), (400,600), 2)
-        pygame.draw.line(display_surf, white, (0,300), (800,300), 2)  
-
-        #Frame
-        pygame.draw.rect(display_surf, white, (a-162, b-102, 324, 204))
-        
-        #Central Frame
-        pygame.draw.rect(display_surf, white, (a-120, b-60, 240, 120))
-
-        #Cetral rectangle
-        pygame.draw.rect(display_surf, black, (a-118, b-68, 236, 136))
+        pygame.draw.line(display_surf, white, (0,300), (800,300), 2)
 
         #side polygon
-        ps1=[a+120, b+60], [a+160, b+100], [a-160, b+100], [a-120, b+60]
-        ps2=[a+120, b-60], [a+160, b-100], [a-160, b-100], [a-120, b-60]
-        ps3=[a+120, b+60], [a+160, b+100], [a+160, b-100], [a+120, b-60]
-        ps4=[a-120, b+60], [a-160, b+100], [a-160, b-100], [a-120, b-60]
-        pygame.draw.polygon(display_surf, gray, ps1)
-        pygame.draw.polygon(display_surf, gray, ps2)
-        pygame.draw.polygon(display_surf, gray, ps3)
-        pygame.draw.polygon(display_surf, gray, ps4)
+        pygame.draw.polygon(display_surf, black, [[a+120, b+60], [a+160, b+100], [a-160, b+100], [a-120, b+60]])
+        pygame.draw.polygon(display_surf, black, [[a+120, b-60], [a+160, b-100], [a-160, b-100], [a-120, b-60]])
+        pygame.draw.polygon(display_surf, black, [[a+120, b+60], [a+160, b+100], [a+160, b-100], [a+120, b-60]])
+        pygame.draw.polygon(display_surf, black, [[a-120, b+60], [a-160, b+100], [a-160, b-100], [a-120, b-60]])
         
         #side line
         pygame.draw.line(display_surf, white, [a+120, b+60], [a+160, b+100], 2)
         pygame.draw.line(display_surf, white, [a+120, b-60], [a+160, b-100], 2)
         pygame.draw.line(display_surf, white, [a-120, b+60], [a-160, b+100], 2)
         pygame.draw.line(display_surf, white, [a-120, b-60], [a-160, b-100], 2)
+
+        #Central Frame
+        pygame.draw.rect(display_surf, white, (a-120, b-60, 240, 120))
+
+        #Cetral rectangle
+        pygame.draw.rect(display_surf, black, (a-118, b-58, 236, 116))
