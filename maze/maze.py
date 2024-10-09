fd782f7e577bea77468013d6a1513dcec9e27ee9
[GithubEF0818] <ef20080118@gmail.com>
2024-10-09 16:32:50 +0900

    최종본
    1. 랜덤 미로 생성 코드를 짰습니다.
    2. 랜덤 미로 생성 코드의 오류 발생 여부를 알 수 없어 임시로 남겨두었던 미로를 선택적으로 플레이할 수 있도록 남겨두었습니다.
    3. 엔딩 화면에서 끝나면 되돌아가는 코드 작성에 어려움이 있어 esc 버튼을 누르면 게임이 종료되도록 바꾸었습니다.

--- maze/maze.py
+++ maze/maze.py
@@ -4,7 +4,7 @@ import random as r
 
 class Maze:
     ##### 맵 시작 알고리즘 #####
-    global mazePath; mazePath = [[[[0 for x in range(21)] for y in range(21)] for z in range(21)] for w in range(21)] 
+    global mazePath; mazePath = [[[[0 for x in range(22)] for y in range(22)] for z in range(22)] for w in range(22)] 
     # 0: 막힌 길, 1: 뚫린 길, 2: 시작점, 3: 도착점
 
     def __init__(self):
@@ -16,56 +16,93 @@ class Maze:
         coords = np.stack(grid, axis=-1).reshape(-1,4)
         coords_list = coords.tolist()
         return coords_list
+    
+    def mazeMode():
+        global modeOfMaze
+        modeOfMaze = input("Random or Stated? (r/s): ")
 
-    ### 시작지점, 도착지점 인수 돌리기 ###
-    global startPos; startPos = r.randrange(20)
-    endPos = r.randrange(20)
+    ##### 랜덤 미로 생성 -> 깊이 우선 탐색(DFS) 알고리즘 이용, 미로크기 홀수배로 해야 오류안나더라고요 21x21x21x21로 설정했습니다 #####
+    
+    global startRmazeX, startRmazeY, startRmazeZ, startRmazeW
+    startRmazeX = r.randrange(1,21,2)
+    startRmazeY = r.randrange(1,21,2)
+    startRmazeZ = r.randrange(1,21,2)
+    startRmazeW = r.randrange(1,21,2)
+    
+    def randomMaze():
+        ### 시작지점 설정 ###
+        mazePath[startRmazeX][startRmazeY][startRmazeZ][startRmazeW] = 2
 
-    ### 시작지점, 도착지점 최단경로 생성 ###
-    for x in range(startPos,20): mazePath[x][0][0][0] = 1
-    for y in range(20): mazePath[19][y][0][0] = 1
-    for z in range(20): mazePath[19][19][z][0] = 1
-    for w in range(endPos): mazePath[19][19][19][w] = 1
+        ### 도착지점 설정 ###
+        while True:
+            endX = r.randrange(1,21,2)
+            endY = r.randrange(1,21,2)
+            endZ = r.randrange(1,21,2)
+            endW = r.randrange(1,21,2)
 
-    ### 시작지점, 도착지점 결정 ###
-    mazePath[startPos][0][0][0] = 2
-    mazePath[19][19][19][endPos] = 3
+            if (endX, endY, endZ, endW) != (startRmazeX, startRmazeY, startRmazeZ, startRmazeW):
+                break
+        
+        mazePath[endX][endY][endZ][endW] = 3
 
-    # ##### 길 생성 알고리즘 #####
-    # ### 길 모델 (추후 임의로 수정 가능) ###
-    # for x in range(0,10): # (0,0,0,0) -> (9,0,0,0)          
-    #     mazePath[x][0][0][0] = 1
-    # for y in range(0,4): # (9,0,0,0) -> (9,3,0,0)          
-    #     mazePath[9][y][0][0] = 1
-    # for x in range(9,13): # (9,3,0,0) -> (12,3,0,0)          
-    #     mazePath[x][3][0][0] = 1
-    # for w in range(0,6): # (12,3,0,0) -> (12,3,0,5)
-    #     mazePath[12][3][0][w] = 1
-    # for z in range(0,8): # (12,3,0,5) -> (12,3,7,5)
-    #     mazePath[12][3][z][5] = 1
-    # for y in range(3,9): # (12,3,7,5) -> (12,8,7,5)
-    #     mazePath[12][y][7][5] = 1
-    # for z in range(4,8): # (12,8,7,5) -> (12,8,4,5)
-    #     mazePath[12][8][z][5] = 1
-    # for w in range(5,12): # (12,8,4,5) -> (12,8,4,11)
-    #     mazePath[12][8][4][w] = 1
-    # for x in range(10,13): # (12,8,4,11) -> (10,8,4,11)
-    #     mazePath[x][8][4][11] = 1
-    # for w in range(11,20): # (12,8,4,11) -> (12,8,4,19)
-    #     mazePath[12][8][4][w] = 1
-    # for x in range(12,16): # (12,8,4,19) -> (15,8,4,19)
-    #     mazePath[x][8][4][19] = 1
-    # for z in range(4,11): # (15,8,4,19) -> (15,8,10,19)
-    #     mazePath[15][8][z][19] = 1
-    # for w in range(14,20): # (15,8,10,19) -> (15,8,10,14)
-    #     mazePath[15][8][10][w] = 1
-    # for y in range(8,16): # (15,8,10,14) -> (15,15,10,14)
-    #     mazePath[15][y][10][14] = 1
-    # for z in range(10,20): # (15,15,10,14) -> (15,15,19,14)
-    #     mazePath[15][15][z][14] = 1
-    # for x in range(15,20): # (15,15,19,14) -> (19,15,19,14)
-    #     mazePath[x][15][19][14] = 1
-    # for y in range(15,20): # (19,15,19,14) -> (19,19,19,14)
-    #     mazePath[19][y][19][14] = 1
-    # for w in range(14,20): # (19,19,19,14) -> (19,19,19,19)
-    #     mazePath[19][19][19][w] = 1
+        ### 시작지점, 도착지점 최단경로 생성 ###
+        visit = [(startRmazeX, startRmazeY, startRmazeZ, startRmazeW)] # 방문한 셀 기록
+        direction = [
+            (0,0,-2,0), (0,0,2,0),
+            (0,-2,0,0), (0,2,0,0),
+            (-2,0,0,0), (2,0,0,0),
+            (0,0,0,-2), (0,0,0,2)
+        ] # 방향 설정
+
+        while visit:
+            x, y, z, w = visit[-1]
+            neighbor = []
+
+            # 이웃 셀 탐색
+            for dx, dy, dz, dw in direction:
+                nx, ny, nz, nw = x+dx, y+dy, z+dz, w+dw
+                if (
+                    0 < nx < 21 and 0 < ny < 21 and 0 < nz < 21 and 0 < nw < 21 and
+                    mazePath[nx][ny][nz][nw] == 0
+                ):
+                    neighbor.append((nx, ny, nz, nw))
+
+            # 무작위 이웃 셀 선택
+            if neighbor:
+                nx, ny, nz, nw = r.choice(neighbor)
+                mazePath[(y+ny) // 2][(x+nx) // 2][(z+nz) // 2][(w+dw) // 2] = 1
+                mazePath[nx][ny][nz][nw] = 1
+                visit.append((nx, ny, nz, nw))
+            else:
+                visit.pop()
+        
+    ##### 정해진 미로 (랜덤미로에 오류가 있을 시) #####
+    def statedMaze():
+        for x in range(0,10):  mazePath[x][0][0][0]    = 1 # (0,0,0,0) -> (9,0,0,0)
+        for y in range(0,4):   mazePath[9][y][0][0]    = 1 # (9,0,0,0) -> (9,3,0,0)          
+        for x in range(9,13):  mazePath[x][3][0][0]    = 1 # (9,3,0,0) -> (12,3,0,0)          
+        for w in range(0,6):   mazePath[12][3][0][w]   = 1 # (12,3,0,0) -> (12,3,0,5)
+        for z in range(0,8):   mazePath[12][3][z][5]   = 1 # (12,3,0,5) -> (12,3,7,5)
+        for y in range(3,9):   mazePath[12][y][7][5]   = 1 # (12,3,7,5) -> (12,8,7,5)
+        for z in range(4,8):   mazePath[12][8][z][5]   = 1 # (12,8,7,5) -> (12,8,4,5)
+        for w in range(5,12):  mazePath[12][8][4][w]   = 1 # (12,8,4,5) -> (12,8,4,11)
+        for x in range(10,13): mazePath[x][8][4][11]   = 1 # (12,8,4,11) -> (10,8,4,11)
+        for w in range(11,20): mazePath[12][8][4][w]   = 1 # (12,8,4,11) -> (12,8,4,20)
+        for x in range(12,16): mazePath[x][8][4][20]   = 1 # (12,8,4,20) -> (15,8,4,20)
+        for z in range(4,11):  mazePath[15][8][z][20]  = 1 # (15,8,4,20) -> (15,8,10,20)
+        for w in range(14,20): mazePath[15][8][10][w]  = 1 # (15,8,10,20) -> (15,8,10,14)
+        for y in range(8,16):  mazePath[15][y][10][14] = 1 # (15,8,10,14) -> (15,15,10,14)
+        for z in range(10,20): mazePath[15][15][z][14] = 1 # (15,15,10,14) -> (15,15,20,14)
+        for x in range(15,20): mazePath[x][15][20][14] = 1 # (15,15,20,14) -> (20,15,20,14)
+        for y in range(15,20): mazePath[20][y][20][14] = 1 # (20,15,20,14) -> (20,20,20,14)
+        for w in range(14,20): mazePath[20][20][20][w] = 1 # (20,20,20,14) -> (20,20,20,20)
+
+        mazePath[0][0][0][0] = 2
+        mazePath[20][20][20][20] = 3
+
+    mazeMode()
+    if modeOfMaze == 'r': randomMaze()
+    elif modeOfMaze == 's': statedMaze()
+    else: print("error!")
+
+    
