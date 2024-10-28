95a61b96734ff6206a08221d4bc98e15d27d5fd9
[GithubEF0818] <ef20080118@gmail.com>
2024-10-28 14:25:31 +0900

    asdf
    asdf

--- map.py
+++ map.py
@@ -1,65 +1,118 @@
 import pygame
 from pygame.locals import *
 from maze.maze import Maze
+from maze.maze import mazePath
 import render
-from main import *
+import numpy as np
 
 def show_map(display_surf):
-    drawList = [[200,150], [600,150], [200,450], [600,450]]
-    gray  = (200, 200, 200)
-    black = (0, 0, 0)
-    white = (255, 255, 255)
-    for a,b in drawList:
-        #Background cell
-        pygame.draw.rect(display_surf, black, (a-200, b-150, 400, 300))
-
-        #Frame
-        pygame.draw.rect(display_surf, white, (a-162, b-102, 324, 204))
+    global px, py, pz, pw
+    def pos_move(self,event):
+        if event.type == pygame.KEYDOWN and self.k == 1:
+            if event.key == pygame.K_LSHIFT or event.key == pygame.K_RSHIFT:
+                self.modifier *= -1
+            if event.key == pygame.K_q:
+                px=px+1
+            if event.key == pygame.K_w: 
+                py=py+1
+            if event.key == pygame.K_e:
+                pz=pz+1
+            if event.key == pygame.K_r:
+                pw=pw+1         
+        if event.type == pygame.KEYDOWN and self.k == 1: 
+            if event.key == pygame.K_LSHIFT or event.key == pygame.K_RSHIFT:    
+                self.modifier *= -1
+            if event.key == pygame.K_q:
+                px=px-1
+            if event.key == pygame.K_w: 
+                py=py-1
+            if event.key == pygame.K_e:
+                pz=pz-1
+            if event.key == pygame.K_r:
+                pw=pw-1
+        pos_move()
         
-        #cross line
-        pygame.draw.line(display_surf, white, (400,0), (400,600), 2)
-        pygame.draw.line(display_surf, white, (0,300), (800,300), 2)
+        drawList = [
+            [200, 150, px+1, py, pz, pw, px-1, py, pz, pw, 1], 
+            [600, 150, px, py+1, pz, pw, px, py-1, pz, pw, 2], 
+            [200, 450, px, py, pz+1, pw, px, py, pz-1, pw, 3], 
+            [600, 450, px, py, pz, pw+1, px, py, pz, pw-1, 4]
+            ]
 
-        #side polygon
-        pygame.draw.polygon(display_surf, black, [[a+110, b+60], [a+160, b+100], [a-160, b+100], [a-110, b+60]])
-        pygame.draw.polygon(display_surf, black, [[a+110, b-60], [a+160, b-100], [a-160, b-100], [a-110, b-60]])
-        pygame.draw.polygon(display_surf, black, [[a+110, b+60], [a+160, b+100], [a+160, b-100], [a+110, b-60]])
-        pygame.draw.polygon(display_surf, black, [[a-110, b+60], [a-160, b+100], [a-160, b-100], [a+110, b-60]])
-        
-        #side line
-        pygame.draw.line(display_surf, white, [a+110, b+60], [a+160, b+100], 2)
-        pygame.draw.line(display_surf, white, [a+110, b-60], [a+160, b-100], 2)
-        pygame.draw.line(display_surf, white, [a-110, b+60], [a-160, b+100], 2)
-        pygame.draw.line(display_surf, white, [a-110, b-60], [a-160, b-100], 2)
+        backup=drawList
+        gray = (180, 180, 180)
+        black= (0, 0, 0)
+        white= (255, 255, 255)
+        for a, b, i1, i2, i3, i4, i5, i6, i7, i8, vi in drawList:
+            c,d=drawList[vi-1][0], drawList[vi-1][1]
+            #비교기
+            if mazePath[i5][i6][i7][i8]==1:
+                pygame.draw.rect(display_surf, black, (c-200, d-150, 400, 300))
+                pygame.draw.rect(display_surf, white, (c-162, d-102, 324, 204))
+                pygame.draw.rect(display_surf, white, (c-110, d-60, 220, 120))   
+            else: 
+                pygame.draw.rect(display_surf, gray, (c-200, d-150, 400, 300))
+                pygame.draw.rect(display_surf, white, (c-162, d-102, 324, 204))
+                pygame.draw.rect(display_surf, white, (c-110, d-60, 220, 120))
+                
+            if mazePath[i1][i2][i3][i4]==1:
+                pygame.draw.rect(display_surf, black, (c-106, d-58, 212, 116))
+            else:
+                pygame.draw.rect(display_surf, gray, (c-106, d-58, 212, 116))
+            for i in range(0,vi):
+                di=drawList[i]
+            for i in range(0,vi):
+                di.remove(drawList[0])
+            drawList.append(di)
+            if mazePath[drawList[0][6]][drawList[0][7]][drawList[0][8]][drawList[0][9]]==1:
+                pygame.draw.polygon(display_surf, black, [[c+110, d-60], [c+160, d-100], [c+160, d+100], [c+110, d+60]])
+            else:
+                pygame.draw.polygon(display_surf, gray, [[c+110, d-60], [c+160, d-100], [c+160, d+100], [c+110, d+60]])
+            if mazePath[drawList[0][1]][drawList[0][2]][drawList[0][3]][drawList[0][4]]==1:
+                pygame.draw.polygon(display_surf, black, [[c-110, d+60], [c-160, d+100], [c-160, d-100], [c-110, d-60]])
+            else:
+                pygame.draw.polygon(display_surf, gray, [[c-110, d+60], [c-160, d+100], [c-160, d-100], [c-110, d-60]])
 
-        #Central Frame
-        pygame.draw.rect(display_surf, white, (a-110, b-60, 220, 120))
+            if mazePath[drawList[1][6]][drawList[1][7]][drawList[1][8]][drawList[1][9]]==1:
+                pygame.draw.polygon(display_surf, black, [[c-110, d+60], [c-160, d+100], [c+160, d+100], [c+110, d+60]])
+            else:
+                pygame.draw.polygon(display_surf, gray, [[c-110, d+60], [c-160, d+100], [c+160, d+100], [c+110, d+60]])
+            if mazePath[drawList[1][1]][drawList[1][2]][drawList[1][3]][drawList[1][4]]==1:
+                pygame.draw.polygon(display_surf, black, [[c+110, d-60], [c+160, d-100], [c-160, d-100], [c-110, d-60]])
+            else:
+                pygame.draw.polygon(display_surf, gray, [[c+110, d-60], [c+160, d-100], [c-160, d-100], [c-110, d-60]])
+            drawList=backup
 
-        #Cetral rectangle
-        pygame.draw.rect(display_surf, black, (a-106, b-58, 212, 116))
+            #십자가
+            pygame.draw.line(display_surf, white, (400,0), (400,600), 2)
+            pygame.draw.line(display_surf, white, (0,300), (800,300), 2)
+            #x표시
+            pn_List=[[1, 1], [1, -1], [-1, 1], [-1, -1]]
+            for x, y in pn_List:
+                pygame.draw.line(display_surf, white, [a+x*110, b+y*60], [a+x*160, b+y*100], 2)
 
-    #pos information
-    pygame.draw.rect(display_surf, white, (310, 250, 180, 100))
-    pygame.draw.rect(display_surf, black, (312, 252, 176, 96))
-    font = pygame.font.Font(None, 40)
-    posTitle = font.render("POSTION", True, white)
-    display_surf.blit(posTitle, (335, 255))
+         #위치 정보
+        pygame.draw.rect(display_surf, white, (310, 250, 180, 100))
+        pygame.draw.rect(display_surf, black, (312, 252, 176, 96))
+        font=pygame.font.Font(None, 40)
+        text=font.render("POSTION", True, white)
+        display_surf.blit(text, (335, 255))
 
-    #Coordinate
-    i = -1
-    center = [[200, 150, 0], [600, 150, 2], [200, 450, 4], [600, 450, 6]]
-    pos_List = [[-10, -10], [-10, -145], [-150, -10], [120,-10], [-10, -90], [-10, 70]]
-    con=["XP", "XN", "YP", "YN", "ZP", "ZN", "WP", "WN"]
-    for p_x, p_y, in pos_List:
-        i += 1
-        for ps_x, ps_y, N in center:
-            font=pygame.font.Font(None, 30)
-            text=font.render(con[N+i], True, white)
-            display_surf.blit(text, (ps_x+p_x, ps_y+p_y))
-        if i == 1:
-            dis=(con[0], con[1])
-            con.remove(con[0])
-            con.remove(con[0])
-            con.extend(dis)
-            i = 0
-    pygame.display.flip()  
+        #좌표 평면 정보
+        i=-1
+        center=[[200, 150, 0], [600, 150, 2], [200, 450, 4], [600, 450, 6]]
+        pos_List=[[-10, -10], [-10, -145], [-150, -10], [120,-10], [-10, -90], [-10, 70]]
+        con=["XP", "XN", "YP", "YN", "ZP", "ZN", "WP", "WN"]
+        for p_x, p_y, in pos_List:
+            i+=1
+            for ps_x, ps_y, N in center:
+                font=pygame.font.Font(None, 30)
+                text=font.render(con[N+i], True, white)
+                display_surf.blit(text, (ps_x+p_x, ps_y+p_y))
+            if i==1:
+                dis=(con[0], con[1])
+                for _ in range(2):
+                    con.remove(con[0])
+                con.extend(dis)
+                i=0
+                pygame.display.flip()
